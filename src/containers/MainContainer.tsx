e5c5ce474caea26b720e88d4d89ae47b8ed3bc35
cteyr123 <cteyr123@gmail.com>
2022-06-28 21:16:18 +0200

    desktop desing finish

--- src/containers/MainContainer.tsx
+++ src/containers/MainContainer.tsx
@@ -11,30 +11,32 @@ const MainContainer = () => {
           <img src={image} alt="" />
         </div>
         <div className="right-container">
-          <div className="tittle">
-            <h5>PERFUME</h5>
-          </div>
-          <div className="sub-tittle">
-            <h2>Gabrielle Essence Eau De Parfum</h2>
-          </div>
-          <div className="text">
-            <p>
-              A floral, solar and voluptuos interpretation composed by Olivier
-              Polge, Perfumer-Creator for the House of CHANEL
-            </p>
-          </div>
-          <div className="price">
-            <div className="actual-price">
-              <h1>$149.99</h1>
+          <div className="inter-right-container">
+            <div className="tittle">
+              <h5>PERFUME</h5>
             </div>
-            <div className="discount">
-              <h5>$169.99</h5>
+            <div className="sub-tittle">
+              <h2>Gabrielle Essence Eau De Parfum</h2>
             </div>
+            <div className="text">
+              <p>
+                A floral, solar and voluptuos interpretation composed by Olivier
+                Polge, Perfumer-Creator for the House of CHANEL
+              </p>
+            </div>
+            <div className="price">
+              <div className="actual-price">
+                <h1>$149.99</h1>
+              </div>
+              <div className="discount">
+                <h5>$169.99</h5>
+              </div>
+            </div>
+            <button type="button">
+              <img className="cover" src={marketCarIcon} alt="" />
+              Add to Cart
+            </button>
           </div>
-          <button type="button">
-            <img src={marketCarIcon} alt="" />
-            Add to Cart
-          </button>
         </div>
       </div>
     </div>
